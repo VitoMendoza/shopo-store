ed5cc19f803647d8f9719fe7937e1792876bc054
Vito Mendoza <6v.mendoza@gmail.com>
2017-10-09 21:08:27 -0400

    Primera version shopo

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'shopo_store',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'tb_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'ezipPkeFKKcrOBaFk6hZom3wCCd93fGjR5LPGJDnQ9pUCTFPTKiooBNU',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-10-09',
+    'locale' => 'es-ES',
+    'cookie_key' => 'vuKPGvBuXh6Feh9KTsMzdgYhKussW4TM7wycgfzRPqZqVf2u1GJ2hL8t',
+    'cookie_iv' => 'h4yZ1FqK',
+    'new_cookie_key' => 'def000000b39029cf5e9ed29c25d87bcebc279ec5a578f729defcbc8665acd9217ed6914cb7b9e5c3188ec22e302f3583d5c6ad433f6c03d26179ec5321a7d82934dfd9a',
+  ),
+);
